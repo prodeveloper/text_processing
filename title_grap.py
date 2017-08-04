400305b348f88898db44dc140e3e31b8d61384a1
Jacob Chencha <jacobchencha@Jacobs-MacBook-Pro.local>
2017-08-04 18:05:51 +0300

    added chapter heading checker

--- title_grap.py
+++ title_grap.py
@@ -1,7 +1,8 @@
+last_two = ''
 with open("temp.txt") as f:
     prev_line = ''
     for line in f:
-        if line.find('title')!=-1:
+        if line.find('title')!= -1 or line.find('chapter') != -1:
             print (last_two)
         last_two = prev_line
         prev_line = line
